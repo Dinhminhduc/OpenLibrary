e118e165fa0b12179571528dbf777fd4b4d528f0
Dinhminhduc <72441314+Dinhminhduc@users.noreply.github.com>
2022-08-11 21:36:39 +0700

    Initial Commit

---
+++ src/test/java/com/team3/openlibrary/OpenLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.openlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
